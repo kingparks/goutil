be7aa60bf403692c4cd0a3028e7e64b1f23fe811
{1570967070@qq.com} <1570967070@qq.com>
2022-12-10 18:31:19 +0800

    设备授权码

--- machine.go
+++ machine.go
@@ -4,6 +4,7 @@ import (
 	"crypto/md5"
 	"fmt"
 	"github.com/astaxie/beego/httplib"
+	"io"
 	"net"
 	"sort"
 	"strings"
@@ -29,16 +30,23 @@ func getMacMD5() string {
 
 // VerifyMachineLicense 验证服务授权
 func VerifyMachineLicense(appName string) bool {
-	res, err := httplib.Get("https://ghproxy.com/https://raw.githubusercontent.com/Jetereting/license-key/main/" + appName + ".txt").String()
-	if err != nil {
-		res, err = httplib.Get("https://raw.githubusercontent.com/Jetereting/license-key/main/" + appName + ".txt").Retries(7).String()
-		if err != nil {
+	resp, err := httplib.Get("https://ghproxy.com/https://raw.githubusercontent.com/Jetereting/license-key/main/" + appName + ".txt").DoRequest()
+	if err != nil || resp.StatusCode != 200 {
+		fmt.Println("ghproxy 失败", resp.StatusCode, err)
+		resp, err = httplib.Get("https://raw.githubusercontent.com/Jetereting/license-key/main/" + appName + ".txt").Retries(2).DoRequest()
+		if err != nil || resp.StatusCode != 200 {
+			fmt.Println("github 失败", resp.StatusCode, err)
 			return false
 		}
 	}
+	bytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
 	macMD5 := getMacMD5()
 	fmt.Println("设备码:", macMD5)
-	if strings.Contains(res, macMD5) {
+	if strings.Contains(string(bytes), macMD5) {
 		return true
 	}
 	return false
