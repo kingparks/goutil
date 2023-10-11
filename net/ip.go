869f68f3bb1d4dee06995dfdecaf6551d9b61cb9
{1570967070@qq.com} <1570967070@qq.com>
2023-10-11 13:14:47 +0800

    添加GetGeoIP

--- net/ip.go
+++ net/ip.go
@@ -2,6 +2,7 @@ package net
 
 import (
 	"github.com/astaxie/beego/httplib"
+	"github.com/tidwall/gjson"
 	"strings"
 )
 
@@ -15,4 +16,13 @@ func GetIPV4() string {
 func GetIP() string {
 	ip, _ := httplib.Get("http://ip.sb").Header("User-Agent", "curl").String()
 	return strings.TrimSpace(ip)
+}
+
+// GetGEO 获取GEO
+func GetGEO(ip ...string) string {
+	geo, _ := httplib.Get("https://api.ip.sb/geoip/"+strings.Join(ip, ",")).
+		Header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36").
+		String()
+	gJsonGeo := gjson.Parse(geo)
+	return gJsonGeo.Get("country").String() + "_" + gJsonGeo.Get("region").String() + "_" + gJsonGeo.Get("city").String()
 }
--- string_test.go
+++ string_test.go
@@ -2,9 +2,14 @@ package goutil
 
 import (
 	"fmt"
+	"github.com/Jetereting/goutil/net"
 	"testing"
 )
 
 func TestStr_GetBetween(t *testing.T) {
 	fmt.Println(Str("1234").GetBetween("1", "232323"))
+}
+
+func TestStr_GEO(t *testing.T) {
+	fmt.Println(net.GetGEO("129.154.205.7"))
 }
